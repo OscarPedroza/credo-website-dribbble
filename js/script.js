2b475a96639369b16c37f51ee27e67857c3bad19
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-11 20:15:54 -0600

    Our team responsive (funcionalidad del slider)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
+/**
+ * Responsive menu
+ */
+
 const btnMenu = document.getElementById('menu-btn');
 const nav = document.getElementById('nav');
 
 btnMenu.addEventListener('click', () => {
   nav.classList.toggle('active');
   btnMenu.classList.toggle('active');
+});
+
+/**
+ * Slider (Our team section)
+ */
+
+const slider = document.getElementById('slider');
+const btnLeft = document.getElementById('btn-left');
+const btnRight = document.getElementById('btn-right');
+
+btnLeft.addEventListener('click', () => {
+  slider.scrollBy({ left: -356, behavior: 'smooth' });
+});
+
+btnRight.addEventListener('click', () => {
+  slider.scrollBy({ left: 356, behavior: 'smooth' });
 });
